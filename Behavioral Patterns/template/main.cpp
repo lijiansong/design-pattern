6ce5cbee6bdbbbd9eed322ac06f8ab958592f98f
Json Lee <jiansongli426@gmail.com>
2017-01-09 22:46:33 +0800

    template

---
+++ Behavioral Patterns/template/main.cpp
@@ -0,0 +1,12 @@
+#include "Template.h"
+#include <iostream>
+using namespace std;
+int main()
+{
+	AbstractClass* p1 = new ConcreteClass1();
+	AbstractClass* p2 = new ConcreteClass2(); 
+	p1->TemplateMethod();
+	p2->TemplateMethod();
+	system("pause");
+	return 0;
+}
