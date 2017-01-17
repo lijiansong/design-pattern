143a19c744d47c99e43bedbadeee8a6b23c5d03d
Json Lee <jiansongli426@gmail.com>
2017-01-17 23:36:44 +0800

    Memento pattern

---
+++ Behavioral Patterns/memento/main.cpp
@@ -0,0 +1,16 @@
+#include "Memento.h"
+#include <iostream>
+using namespace std;
+int main()
+{
+	Originator* o = new Originator();
+	o->SetState("old"); //±¸ÍüÇ°×´Ì¬
+	o->PrintState();
+	Memento* m = o->CreateMemento(); //½«×´Ì¬±¸Íü
+	o->SetState("new"); //ÐÞ¸Ä×´Ì¬
+	o->PrintState();
+	o->RestoreToMemento(m); //»Ö¸´ÐÞ¸ÄÇ°×´Ì¬
+	o->PrintState();
+	system("pause");
+	return 0;
+}
