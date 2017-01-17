e194a527317366133f44e9ae586617a44b4f31b3
Json Lee <jiansongli426@gmail.com>
2017-01-17 23:04:41 +0800

    observer

---
+++ Behavioral Patterns/observer/Subject.cpp
@@ -0,0 +1,46 @@
+#include "Subject.h" 
+#include "Observer.h"
+#include <iostream>
+#include <list>
+using namespace std;
+typedef string State;
+Subject::Subject() 
+{
+	_obvs = new list<Observer*>;
+}
+Subject::~Subject()
+{}
+void Subject::Attach(Observer* obv)
+{
+	_obvs->push_front(obv);
+}
+void Subject::Detach(Observer* obv)
+{
+	if (obv != NULL) 
+		_obvs->remove(obv);
+}
+void Subject::Notify()
+{
+	list<Observer*>::iterator it,ie;
+	it = _obvs->begin();
+	ie = _obvs->end();
+	for (; it != ie; ++it) 
+	{
+		(*it)->Update(this);
+	}
+}
+
+ConcreteSubject::ConcreteSubject()
+{
+	_st = '\0';
+}
+ConcreteSubject::~ConcreteSubject()
+{}
+State ConcreteSubject::GetState()
+{
+	return _st;
+}
+void ConcreteSubject::SetState(const State& st)
+{
+	_st = st;
+}
---
+++ Behavioral Patterns/observer/main.cpp
@@ -0,0 +1,16 @@
+#include "Subject.h"
+#include "Observer.h"
+#include <iostream> 
+using namespace std;
+int main()
+{
+	ConcreteSubject* sub = new ConcreteSubject();
+	Observer* o1 = new ConcreteObserverA(sub);
+	Observer* o2 = new ConcreteObserverB(sub);
+	sub->SetState("old");
+	sub->Notify();
+	sub->SetState("new"); //也可以由Observer调用
+	sub->Notify();
+	system("pause");
+	return 0;
+}
